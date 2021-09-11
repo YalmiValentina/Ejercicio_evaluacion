7ce2b147d6a397970c8d8830237fe842ba341c99
YalmiValentina <yalmivale@gmail.com>
2021-09-10 22:07:01 -0300

    adjuntar las variables asociadas a un participante

---
+++ participante.rb
@@ -0,0 +1,18 @@
+class Participante 
+    attr_accessor  :codigo, :nombre, :edad, :dni, :colorCinturon, :partidasGanadas, :partidasEmpatadas, :partidasPerdidas
+    def initialize codigo, nombre, edad, dni, colorCinturon, partidasGanadas,partidasEmpatadas, partidasPerdidas
+        @codigo = codigo
+        @nombre = nombre
+        @edad = edad
+        @dni = dni
+        @colorCinturon = colorCinturon
+        @partidasGanadas = partidasGanadas
+        @partidasEmpatadas = partidasEmpatadas
+        @partidasPerdidas = partidasPerdidas        
+        @puntaje = (@partidasGanadas) * 4 + (@partidasEmpatadas)  * 3
+    end
+    
+    def nuevo_puntaje
+        @puntaje = (@partidasGanadas) * 4 + (@partidasEmpatadas)  * 3
+    end
+end
